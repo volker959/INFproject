cddecdd169ca4c0b46a69143718c9ea3afdc6e0e
Damian <damian0207@gmail.com>
2019-04-08 22:45:01 +0200

    footer + strona glowna, dodanie newsow

--- header.php
+++ header.php
@@ -5,11 +5,14 @@
     <title>INFRACTION GAMING</title>
     
     <link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+
 
     <script type="text/javascript" src="js/swiper.min.js"></script>
     <script src="js/main.js"></script>
     <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
 
 </head>
 <body>
--- index.php
+++ index.php
@@ -25,8 +25,7 @@
         <li>N</li>
         <li>G</li>
         </ul>
-
-            <div class="svg-wrapper enter-button">
+        <div class="svg-wrapper enter-button">
             <svg height="39" width="150" xmlns="http://www.w3.org/2000/svg">
                 <rect id="shape" height="40" width="150" />
                 <div id="text">
@@ -34,7 +33,6 @@
                 </div>
             </svg>
             </div>
-
     </div>
 </div>
 
@@ -65,68 +63,93 @@
         <div class="swiper-pagination"></div>
     </div>
 </div>
-
-<div class="news-box">
-<?php
-           
-            $dbh = connect_to_db();
-            $news = fetchNews($dbh);
-        ?>
-
-        <?php if ( $news && !empty($news) ) :?>
-
-        <?php foreach ($news as $key => $article) :?>
-        <h2><a href="read-news.php?newsid=<?= $article->news_id ?>"><?= stripslashes($article->news_title) ?></a></h2>
-        <p><?= stripslashes($article->news_short_desc) ?></p>
-        <span>published on <?= date("M, jS  Y, H:i", $article->news_published_on) ?> by <?= stripslashes($article->news_author) ?></span>
-        <?php endforeach?>
-
-        <?php endif?>
-
-
+<div id="bialo" class="container-fluid">
+    <div class="container full-vh">
+        <div class="row">
+            <div class="col-lg-10 news-hello">
+                <h1>OSTANIE NEWSY</h1>
+                <h4>Bądz zawsze na bieżąco</h4>
+            </div>
+            <div class="col-lg-2">
+                <button type="button" class="btn btn-outline-primary btn-lg all-news">WSZYSTKIE NEWSY</button>
+            </div>
+            <div class="col-lg-6 news-top news1">
+                <h1>Tytuł newsa CSGO</h1>
+                <p>Opis newsa wraz z krótkim rozwinięciem</p>
+            </div>
+            <div class="col-lg-6 news-top news2">
+                <h1>Pedro na podium</h1>
+                <p>Opis newsa wraz z krótkim rozwinięciem</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 news-bottom news3">
+                <h1>Tytuł newsa CSGO</h1>
+                <p>Opis newsa wraz z krótkim rozwinięciem</p>
+            </div>
+            <div class="col-lg-4 news-bottom news4">
+                <h1>Tytuł newsa CSGO</h1>
+                <p>Opis newsa wraz z krótkim rozwinięciem</p>
+            </div>
+            <div class="col-lg-4 news-bottom news5">
+                <h1>Tytuł newsa</h1>
+                <p>Opis newsa wraz z krótkim rozwinięciem</p>
+            </div>
+        </div>
+    </div>
 </div>
+
 <?php 
     include 'facebook-box.php';
-/*
-    include 'content.php';
-    include 'footer.php'; 
-*/
 ?>
+</div>
+<div class="container">
+    <div class="row">
+		<div class="col-lg-12 army">
+            <div class="army-text">
+                <h1>SPOTKAJ NASZĄ ARMIĘ</h1>
+            </div>
+		</div>
+    </div>
+    <div class="break"></div>
+</div>
 
-<div class="army">
-    <div class="army-text">
-        <h1>MEET OUR ARMY</h1>
+<div class="container">
+    <div class="row">
+		<div class="col-lg-4 army">
+            <div class="">
+                <h2 class="big-text">5<br>DRUŻYN</h2>
+            </div>
+		</div>
+        <div class="col-lg-4 army">
+            <div class="">
+                <h2>21<br>GRACZY</h2>
+            </div>
+		</div>
+        <div class="col-lg-4 army">
+            <div class="">
+                <h2>36<br>NAGRÓD</h2>
+            </div>
+		</div>
+    </div>
+    <div class="break"></div>
+    <div class="row teams">
+        <div class="col-lg-2">
+            <div>
+            <button type="button" class="btn btn-outline-primary btn-lg teams-link-button">WSZYSTKIE DRUŻYNY</button>
+            </div>
+        </div>
+        <div class="col-lg-8 teams-link">
+            <h1>NASZE DYWIZJE</h1>
+        </div>
+        <div class="col-lg-2">
+
+        </div>
     </div>
 </div>
 
-<div class="break"></div>
-
-<div class="cerafix">
-    <div calss="box">
-        <h2>5<br>TEAMS</h2>
-    </div>
-    <div calss="box">
-        <h2>21<br>PLAYERS</h2>
-    </div>
-    <div calss="box">
-        <h2>36<br>AWARDS</h2>
-    </div>
-</div>
-
-
-<!--
-    <div class="content">
-        <div class="box">
-            tutej jakas zawartosc zrobimy
-        </div>
-        <div class="sidebar">
-            tutaj zrobimy jakies obrazki
-        </div>
-    </div>
-    <div class="footer">
-        zawartosc stopki
-    </div>
--->
-
+<?php
+include 'footer.php';
+?>
 </body>
 </html>
