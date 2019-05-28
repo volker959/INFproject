5fbd8d6a07c046509514678219f1a574a674101d
Damian <damian0207@gmail.com>
2019-05-28 20:30:19 +0200

    usuwanie smieci

--- login.php
+++
@@ -1,4 +0,0 @@
-<div class="socials">
-    <a href="https://www.facebook.com/infractiongaming/" target="_blank"><img src="img/facebook.png"></a>
-    <a href="https://www.youtube.com/" target="_blank"><img src="img/youtube.png"></a>
-</div>
